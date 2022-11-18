bfb6ef5554de89f5d1f55f4b3649a42e90d7a8df
Guilherme <guilherme.silva@mpt.mp.br>
2022-11-18 14:34:49 -0300

    social media learning - first

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+import express from "express"
+import {
+    getUser,
+    getUserFriends,
+    addRemoveFriend,
+} from "../controllers/users.js"
+
+const router = express.Router()
+
+/* READ */
+router.get("/:id", verifyToken, getUser)
