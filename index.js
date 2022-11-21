99ef715fb7632136f943ddee4dcf3b87373c41db
Guilherme <guilherme.silva@mpt.mp.br>
2022-11-21 15:52:56 -0300

    posts and mock data

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ import { register } from "./controllers/auth.js"
 import { createPost } from "./controllers/posts.js"
 import { verifyToken } from "./middleware/auth.js"
 
+import User from "./models/User.js"
+import Post from "./models/Post.js"
+import { users, posts } from "./data/index.js"
+
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -59,5 +63,7 @@ mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(()=>{
+    // User.insertMany(users)
+    // Post.insertMany(posts)
     app.listen(PORT, ()=>console.log(`Server PORT: ${PORT}`))
 }).catch((error)=>console.error(error))
